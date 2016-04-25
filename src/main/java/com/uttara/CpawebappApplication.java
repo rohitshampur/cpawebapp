b4f64797792a52a6a0fff97cddc8cc941908e505
Rohit <rohit@uttarainfo.com>
2016-04-25 10:23:47 +0530

    initial commit

---
+++ src/main/java/com/uttara/CpawebappApplication.java
@@ -0,0 +1,12 @@
+package com.uttara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpawebappApplication.class, args);
+	}
+}
